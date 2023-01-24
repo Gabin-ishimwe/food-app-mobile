9506bed892b7360602de3cd2426cfa4715c02ac2
Gabin-ishimwe <g.ishimwe@alustudent.com>
2023-01-24 13:07:53 +0200

    onboard layout page

--- lib/screens/onboarding_screen.dart
+++ lib/screens/onboarding_screen.dart
@@ -20,33 +20,96 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
         child:
             Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: [
           Expanded(
-            flex: 7,
+            flex: 6,
             child: Container(
-              color: Color.fromARGB(255, 255, 234, 233),
-              padding: EdgeInsets.all(16),
+              // color: Color.fromARGB(255, 255, 234, 233),
+              padding: EdgeInsets.all(18),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10.0),
+                color: Color.fromARGB(255, 255, 234, 233),
+              ),
               child: Column(children: [
-                Text(
-                  "Skip",
-                  style: GoogleFonts.poppins(
-                      textStyle: TextStyle(
-                    color: Color.fromARGB(255, 235, 53, 34),
-                  )),
+                Align(
+                  child: Text(
+                    "Skip",
+                    style: GoogleFonts.poppins(
+                        textStyle: TextStyle(
+                      color: Color.fromARGB(255, 235, 53, 34),
+                      fontSize: 16,
+                    )),
+                  ),
+                  alignment: Alignment.topRight,
                 ),
-                SvgPicture.asset(
-                  "images/onboarding/online_groceries.svg",
-                  semanticsLabel: "logo",
-                  height: 200,
-                  width: 200,
-                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(vertical: 20),
+                  child: SvgPicture.asset(
+                    "images/onboarding/online_groceries.svg",
+                    semanticsLabel: "logo",
+                    height: 250,
+                    width: 250,
+                  ),
+                )
+                // Row(
+                //   children: [
+
+                //   ],
+                // )
               ]),
             ),
           ),
           Expanded(
-            flex: 3,
-            child: Container(
-              color: Colors.yellow,
-            ),
-          ),
+              flex: 4,
+              child: Padding(
+                padding: EdgeInsets.symmetric(vertical: 20),
+                child: Container(
+                  child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      mainAxisSize: MainAxisSize.max,
+                      // mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 20),
+                          child: Text(
+                            "Shop from your favorite restaurant",
+                            style: GoogleFonts.poppins(
+                                textStyle: TextStyle(
+                                    fontSize: 26, fontWeight: FontWeight.bold)),
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                        Padding(
+                          padding:
+                              EdgeInsets.symmetric(horizontal: 20, vertical: 8),
+                          child: Text(
+                            "A diverse list of different dining restaurants throughout the territory and around your area carefully selected",
+                            style: GoogleFonts.poppins(
+                                textStyle: TextStyle(
+                                    color: Color.fromARGB(255, 150, 150, 150))),
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                        Spacer(),
+                        ElevatedButton(
+                          onPressed: () {},
+                          child: Text(
+                            "Next",
+                            style: GoogleFonts.poppins(
+                                textStyle: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white,
+                                ),
+                                fontSize: 20),
+                          ),
+                          style: ElevatedButton.styleFrom(
+                              backgroundColor: Color.fromARGB(255, 235, 53, 34),
+                              padding: EdgeInsets.all(14),
+                              shape: RoundedRectangleBorder(
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(8)))),
+                        ),
+                      ]),
+                ),
+              )),
         ]),
       ),
     ));
