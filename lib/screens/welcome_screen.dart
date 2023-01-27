27b8b5e1a9a33434a8753a1bf43d2cc29396390d
Gabin-ishimwe <g.ishimwe@alustudent.com>
2023-01-27 11:54:19 +0200

    ft/start welcome page

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -12,15 +14,92 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-          child: Text(
-        "Welcome Page",
-        style: GoogleFonts.poppins(
-            textStyle: TextStyle(
-          color: Color.fromARGB(255, 235, 53, 34),
-          fontSize: 16,
-        )),
-      )),
+      body: Container(
+          margin: EdgeInsets.only(top: 35, right: 20, left: 20, bottom: 20),
+          child: Column(
+            children: [
+              Expanded(
+                child: Container(
+                  padding: EdgeInsets.all(30),
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      image: DecorationImage(
+                          fit: BoxFit.cover,
+                          image: AssetImage(
+                              "images/welcome_screen/background.png"))),
+                  child: Column(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Text(
+                          "A wonderful serenity taken possession",
+                          style: GoogleFonts.poppins(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 26),
+                        ),
+                        Padding(padding: EdgeInsets.symmetric(vertical: 4)),
+                        Text(
+                          "I am alone, and feel the charm of existence in this spot, which was created for the bliss",
+                          style: GoogleFonts.poppins(
+                              color: Colors.white,
+                              fontWeight: FontWeight.normal,
+                              fontSize: 16),
+                        )
+                      ]),
+                ),
+                flex: 7,
+              ),
+              Padding(padding: EdgeInsets.symmetric(vertical: 8)),
+              Expanded(
+                child: Container(
+                  // width: double.infinity,
+                  child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        ElevatedButton(
+                          onPressed: () {},
+                          child: Text(
+                            "Create a new account",
+                            style: GoogleFonts.poppins(
+                                textStyle: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white,
+                                ),
+                                fontSize: 18),
+                          ),
+                          style: ElevatedButton.styleFrom(
+                              backgroundColor: Color.fromARGB(255, 235, 53, 34),
+                              padding: EdgeInsets.all(14),
+                              shape: RoundedRectangleBorder(
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(8)))),
+                        ),
+                        Padding(padding: EdgeInsets.symmetric(vertical: 8)),
+                        OutlinedButton(
+                          onPressed: () {},
+                          child: Text(
+                            "Sign in",
+                            style: GoogleFonts.poppins(
+                                color: Color.fromARGB(255, 235, 53, 34),
+                                fontWeight: FontWeight.bold,
+                                fontSize: 18),
+                          ),
+                          style: OutlinedButton.styleFrom(
+                              backgroundColor: Colors.white,
+                              side: BorderSide(
+                                  color: Color.fromARGB(255, 235, 53, 34),
+                                  width: 2),
+                              padding: EdgeInsets.all(14),
+                              shape: RoundedRectangleBorder(
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(8)))),
+                        )
+                      ]),
+                ),
+                flex: 3,
+              )
+            ],
+          )),
     );
   }
 }
