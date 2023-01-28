b2ab1353c79128d08075fd188fa472fe7f6f114e
Gabin-ishimwe <g.ishimwe@alustudent.com>
2023-01-28 23:00:11 +0200

    ft/animate menu buttons

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -11,149 +11,178 @@ class WelcomeScreen extends StatefulWidget {
 }
 
 class _WelcomeScreenState extends State<WelcomeScreen> {
+  bool showBottomMenu = false;
+  var thresholdScroll =
+      100; // min pixel/second to be considered as screen scroll
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-        child: Scaffold(
-            body: Stack(
-      children: [
-        Container(
-            margin: EdgeInsets.all(20),
-            child: Column(
-              children: [
-                Expanded(
-                  flex: 7,
-                  child: Container(
-                    padding: EdgeInsets.all(30),
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        image: DecorationImage(
-                            fit: BoxFit.cover,
-                            image: AssetImage(
-                                "images/welcome_screen/background.png"))),
-                    child: Column(
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        children: [
-                          Text(
-                            "A wonderful serenity taken possession",
-                            style: GoogleFonts.poppins(
-                                color: Colors.white,
-                                fontWeight: FontWeight.bold,
-                                fontSize: 26),
-                          ),
-                          Padding(padding: EdgeInsets.symmetric(vertical: 4)),
-                          Text(
-                            "I am alone, and feel the charm of existence in this spot, which was created for the bliss",
-                            style: GoogleFonts.poppins(
-                                color: Colors.white,
-                                fontWeight: FontWeight.normal,
-                                fontSize: 16),
-                          )
-                        ]),
-                  ),
-                ),
-                Padding(padding: EdgeInsets.symmetric(vertical: 8)),
-                Expanded(
-                  flex: 3,
-                  child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
-                      children: [
-                        ElevatedButton(
-                          onPressed: () {},
-                          style: ElevatedButton.styleFrom(
-                              backgroundColor: Color.fromARGB(255, 235, 53, 34),
-                              padding: EdgeInsets.all(14),
-                              shape: RoundedRectangleBorder(
-                                  borderRadius:
-                                      BorderRadius.all(Radius.circular(8)))),
-                          child: Text(
-                            "Create a new account",
-                            style: GoogleFonts.poppins(
-                                textStyle: TextStyle(
-                                  fontWeight: FontWeight.bold,
+        child: GestureDetector(
+      onPanEnd: (details) {
+        // scroll down pixelPerSecond increase
+        if (details.velocity.pixelsPerSecond.dy > thresholdScroll) {
+          setState(() {
+            showBottomMenu = true;
+          });
+        }
+        // scroll up pixelPerSecond descrease
+        else if (details.velocity.pixelsPerSecond.dy < -thresholdScroll) {
+          setState(() {
+            showBottomMenu = false;
+          });
+        }
+      },
+      child: Scaffold(
+          body: Stack(
+        children: [
+          Container(
+              margin: EdgeInsets.all(20),
+              child: Column(
+                children: [
+                  Expanded(
+                    flex: 7,
+                    child: Container(
+                      padding: EdgeInsets.all(30),
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          image: DecorationImage(
+                              fit: BoxFit.cover,
+                              image: AssetImage(
+                                  "images/welcome_screen/background.png"))),
+                      child: Column(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            Text(
+                              "A wonderful serenity taken possession",
+                              style: GoogleFonts.poppins(
                                   color: Colors.white,
-                                ),
-                                fontSize: 18),
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 26),
+                            ),
+                            Padding(padding: EdgeInsets.symmetric(vertical: 4)),
+                            Text(
+                              "I am alone, and feel the charm of existence in this spot, which was created for the bliss",
+                              style: GoogleFonts.poppins(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.normal,
+                                  fontSize: 16),
+                            )
+                          ]),
+                    ),
+                  ),
+                  Padding(padding: EdgeInsets.symmetric(vertical: 8)),
+                  Expanded(
+                    flex: 3,
+                    child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: [
+                          ElevatedButton(
+                            onPressed: () {},
+                            style: ElevatedButton.styleFrom(
+                                backgroundColor:
+                                    Color.fromARGB(255, 235, 53, 34),
+                                padding: EdgeInsets.all(14),
+                                shape: RoundedRectangleBorder(
+                                    borderRadius:
+                                        BorderRadius.all(Radius.circular(8)))),
+                            child: Text(
+                              "Create a new account",
+                              style: GoogleFonts.poppins(
+                                  textStyle: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white,
+                                  ),
+                                  fontSize: 18),
+                            ),
                           ),
-                        ),
-                        Padding(padding: EdgeInsets.symmetric(vertical: 8)),
-                        OutlinedButton(
-                          onPressed: () {},
-                          style: OutlinedButton.styleFrom(
-                              backgroundColor: Colors.white,
-                              side: BorderSide(
+                          Padding(padding: EdgeInsets.symmetric(vertical: 8)),
+                          OutlinedButton(
+                            onPressed: () {},
+                            style: OutlinedButton.styleFrom(
+                                backgroundColor: Colors.white,
+                                side: BorderSide(
+                                    color: Color.fromARGB(255, 235, 53, 34),
+                                    width: 2),
+                                padding: EdgeInsets.all(14),
+                                shape: RoundedRectangleBorder(
+                                    borderRadius:
+                                        BorderRadius.all(Radius.circular(8)))),
+                            child: Text(
+                              "Sign in",
+                              style: GoogleFonts.poppins(
                                   color: Color.fromARGB(255, 235, 53, 34),
-                                  width: 2),
-                              padding: EdgeInsets.all(14),
-                              shape: RoundedRectangleBorder(
-                                  borderRadius:
-                                      BorderRadius.all(Radius.circular(8)))),
-                          child: Text(
-                            "Sign in",
-                            style: GoogleFonts.poppins(
-                                color: Color.fromARGB(255, 235, 53, 34),
-                                fontWeight: FontWeight.bold,
-                                fontSize: 18),
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 18),
+                            ),
                           ),
-                        ),
-                        Padding(padding: EdgeInsets.symmetric(vertical: 8)),
-                        OutlinedButton(
-                          onPressed: () {},
-                          style: OutlinedButton.styleFrom(
-                              backgroundColor: Colors.white,
-                              side: BorderSide(
-                                  color: Color.fromARGB(255, 171, 171, 171),
-                                  width: 2),
-                              padding: EdgeInsets.all(14),
-                              shape: RoundedRectangleBorder(
-                                  borderRadius:
-                                      BorderRadius.all(Radius.circular(8)))),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Image(
-                                  image: AssetImage(
-                                      "images/welcome_screen/google.png")),
-                              Padding(
-                                  padding: EdgeInsets.symmetric(horizontal: 6)),
-                              Text(
-                                "Continue with Google",
-                                style: GoogleFonts.poppins(
-                                    textStyle: TextStyle(
-                                        fontWeight: FontWeight.bold,
-                                        fontSize: 18,
-                                        color: Colors.black)),
-                              ),
-                            ],
-                          ),
-                        ),
-                        // Padding(padding: EdgeInsets.symmetric(vertical: 8)),
-                        Align(
-                            alignment: Alignment.center,
-                            child: TextButton(
-                              style: TextButton.styleFrom(
-                                  // foregroundColor: Colors.white,
-                                  splashFactory: NoSplash.splashFactory),
-                              onPressed: (() {}),
-                              child: Text(
-                                "Show more",
-                                style: GoogleFonts.poppins(
-                                    fontSize: 18,
+                          Padding(padding: EdgeInsets.symmetric(vertical: 8)),
+                          OutlinedButton(
+                            onPressed: () {},
+                            style: OutlinedButton.styleFrom(
+                                backgroundColor: Colors.white,
+                                side: BorderSide(
                                     color: Color.fromARGB(255, 171, 171, 171),
-                                    fontWeight: FontWeight.w500),
-                              ),
-                            ))
-                      ]),
-                )
-              ],
-            )),
-        // menu widgets
-        Positioned(
-          child: SocialMenuWidget(),
-          bottom: -(MediaQuery.of(context).size.height),
-        )
-      ],
-    )));
+                                    width: 2),
+                                padding: EdgeInsets.all(14),
+                                shape: RoundedRectangleBorder(
+                                    borderRadius:
+                                        BorderRadius.all(Radius.circular(8)))),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Image(
+                                    image: AssetImage(
+                                        "images/welcome_screen/google.png")),
+                                Padding(
+                                    padding:
+                                        EdgeInsets.symmetric(horizontal: 6)),
+                                Text(
+                                  "Continue with Google",
+                                  style: GoogleFonts.poppins(
+                                      textStyle: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 18,
+                                          color: Colors.black)),
+                                ),
+                              ],
+                            ),
+                          ),
+                          // Padding(padding: EdgeInsets.symmetric(vertical: 8)),
+                          Align(
+                              alignment: Alignment.center,
+                              child: TextButton(
+                                style: TextButton.styleFrom(
+                                    // foregroundColor: Colors.white,
+                                    splashFactory: NoSplash.splashFactory),
+                                onPressed: (() {
+                                  setState(() {
+                                    showBottomMenu = true;
+                                  });
+                                }),
+                                child: Text(
+                                  "Show more",
+                                  style: GoogleFonts.poppins(
+                                      fontSize: 18,
+                                      color: Color.fromARGB(255, 171, 171, 171),
+                                      fontWeight: FontWeight.w500),
+                                ),
+                              ))
+                        ]),
+                  )
+                ],
+              )),
+          Opacity(opacity: showBottomMenu ? 1.0 : 0),
+          // menu widgets
+          AnimatedPositioned(
+            curve: Curves.easeInOut,
+            duration: Duration(milliseconds: 200),
+            child: SocialMenuWidget(),
+            bottom:
+                showBottomMenu ? -20 : -(MediaQuery.of(context).size.height),
+          )
+        ],
+      )),
+    ));
   }
 }
 
@@ -168,12 +197,13 @@ class SocialMenuWidget extends StatelessWidget {
       borderRadius: BorderRadius.circular(20),
       child: Container(
         // decoration: BoxDecoration(borderRadius: BorderRadius.circular(20)),
-        color: Colors.grey,
+        color: Colors.white,
         padding: EdgeInsets.all(20),
         width: width,
         //height: height / 2, // take 1/3 height of the screen
         child:
             Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: [
+          // Padding(padding: EdgeInsets.symmetric(vertical: 8)),
           Padding(
             padding: EdgeInsets.symmetric(horizontal: 40),
             child: Text(
@@ -309,7 +339,8 @@ class SocialMenuWidget extends StatelessWidget {
                       color: Color.fromARGB(255, 171, 171, 171),
                       fontWeight: FontWeight.w500),
                 ),
-              ))
+              )),
+          Padding(padding: EdgeInsets.symmetric(vertical: 8))
         ]),
       ),
     );
