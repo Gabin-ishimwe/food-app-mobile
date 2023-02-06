5d1e1557e11cbcb653daeffc222aa645fd3bf58a
Gabin-ishimwe <g.ishimwe@alustudent.com>
2023-02-06 13:37:42 +0200

    ft-start home appbar

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -12,51 +12,110 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return SafeArea(
         child: Scaffold(
-      // appBar: AppBar(
-      //   title: ,
-      // ),
+      appBar: customAppBar(),
       body: SingleChildScrollView(
         child: Container(
           margin: EdgeInsets.all(20),
-          child: Column(children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Row(
-                  children: [
-                    Icon(
-                      Icons.emoji_emotions,
-                      color: Color(0xFFE8362B),
-                    ),
-                    Padding(padding: EdgeInsets.symmetric(horizontal: 5)),
-                    Text(
-                      "Hello, Daniels!",
-                      style:
-                          TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
-                    )
-                  ],
-                ),
-                Icon(
-                  Icons.notifications_active,
-                  color: Color(0xFFE8362B),
-                )
-              ],
-            ),
-            Padding(padding: EdgeInsets.symmetric(vertical: 20)),
-            Row(
-              // mainAxisSize: MainAxisSize.max,
-              children: [
-                LayoutBuilder(builder: (context, boxConstraint) {
-                  return Container(
-                    width: boxConstraint.maxWidth * 0.8,
-                    color: Colors.red,
-                  );
-                })
-              ],
-            )
+          child: Column(children: const [
+            // Row(
+            //   mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            //   children: [
+            //     Row(
+            //       children: [
+            //         Icon(
+            //           Icons.emoji_emotions,
+            //           color: Color(0xFFE8362B),
+            //         ),
+            //         Padding(padding: EdgeInsets.symmetric(horizontal: 5)),
+            //         Text(
+            //           "Hello, Daniels!",
+            //           style:
+            //               TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
+            //         )
+            //       ],
+            //     ),
+            //     Icon(
+            //       Icons.notifications_active,
+            //       color: Color(0xFFE8362B),
+            //     )
+            //   ],
+            // ),
           ]),
         ),
       ),
     ));
+  }
+
+  PreferredSize customAppBar() {
+    return PreferredSize(
+        preferredSize: Size.fromHeight(200),
+        child: Container(
+          color: Color(0xFFF55349),
+          padding: EdgeInsets.all(20),
+          child: Stack(children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  children: const [
+                    Icon(
+                      Icons.emoji_emotions,
+                      color: Colors.white,
+                    ),
+                    Padding(padding: EdgeInsets.symmetric(horizontal: 5)),
+                    Text(
+                      "Hello, Daniels!",
+                      style: TextStyle(
+                          fontWeight: FontWeight.w600, color: Colors.white),
+                    )
+                  ],
+                ),
+                Row(
+                  children: [
+                    IconButton(
+                      onPressed: () {
+                        print("Hello");
+                      },
+                      icon: Icon(
+                        Icons.notifications_active,
+                        color: Colors.white,
+                      ),
+                    ),
+                    // Padding(padding: EdgeInsets.only()),
+                    IconButton(
+                      onPressed: () {},
+                      icon: Icon(
+                        Icons.menu,
+                        color: Colors.white,
+                      ),
+                    )
+                  ],
+                )
+              ],
+            ),
+            Positioned(
+              // bottom: 0,
+              // top: 50,
+              child: TextFormField(
+                decoration: InputDecoration(
+                    prefixIcon: Icon(
+                      Icons.search,
+                      color: Colors.grey,
+                    ),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: BorderSide(
+                            width: 0,
+                            color: Colors.white,
+                            style: BorderStyle.none)),
+                    fillColor: Colors.white,
+                    filled: true,
+                    hintText: "Search dish name, restaurant",
+                    contentPadding: EdgeInsets.all(10),
+                    hintStyle: TextStyle(fontSize: 14)),
+              ),
+            )
+          ]),
+        ));
   }
 }
