fda50ffef8199c5bd9e84fead861cfdf21cd0da0
Gabin-ishimwe <g.ishimwe@alustudent.com>
2023-02-08 13:49:06 +0200

    fix-nav bar error

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -15,7 +15,7 @@ class _HomeScreenState extends State<HomeScreen> {
       appBar: customAppBar(),
       body: SingleChildScrollView(
         child: Container(
-          margin: EdgeInsets.all(20),
+          margin: const EdgeInsets.all(20),
           child: Column(children: const [
             // Row(
             //   mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -48,14 +48,14 @@ class _HomeScreenState extends State<HomeScreen> {
 
   PreferredSize customAppBar() {
     return PreferredSize(
-        preferredSize: Size.fromHeight(200),
-        child: Container(
-          color: Color(0xFFF55349),
-          padding: EdgeInsets.all(20),
-          height: double.infinity,
-          // width: double.infinity,
-          child: Stack(clipBehavior: Clip.none, children: [
-            Row(
+        preferredSize: const Size.fromHeight(100),
+        child: Stack(clipBehavior: Clip.none, children: [
+          Container(
+            width: MediaQuery.of(context).size.width,
+            color: const Color(0xFFF55349),
+            padding:
+                const EdgeInsets.only(left: 20, right: 20, top: 30, bottom: 40),
+            child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 Row(
@@ -72,52 +72,86 @@ class _HomeScreenState extends State<HomeScreen> {
                     )
                   ],
                 ),
-                Row(
-                  children: [
-                    IconButton(
-                      onPressed: () {
-                        print("Hello");
-                      },
-                      icon: Icon(
-                        Icons.notifications_active,
-                        color: Colors.white,
+                IntrinsicHeight(
+                  child: Row(
+                    mainAxisSize: MainAxisSize.max,
+                    children: [
+                      IconButton(
+                        style: IconButton.styleFrom(),
+                        padding: EdgeInsets.all(0),
+                        constraints: BoxConstraints(),
+                        onPressed: () {
+                          print("Hello");
+                        },
+                        icon: const Icon(
+                          Icons.notifications_active,
+                          color: Colors.white,
+                          size: 22,
+                        ),
                       ),
-                    ),
-                    // Padding(padding: EdgeInsets.only()),
-                    IconButton(
-                      onPressed: () {},
-                      icon: Icon(
-                        Icons.menu,
+                      VerticalDivider(
+                        // thickness: 10,
                         color: Colors.white,
+                        indent: 5,
+                        endIndent: 5,
                       ),
-                    )
-                  ],
+                      IconButton(
+                        padding: EdgeInsets.zero,
+                        constraints: BoxConstraints(),
+                        onPressed: () {
+                          print("menu");
+                        },
+                        icon: const Icon(
+                          Icons.menu,
+                          color: Colors.white,
+                          size: 22,
+                        ),
+                      ),
+                      Padding(padding: EdgeInsets.only(right: 10))
+                    ],
+                  ),
                 )
               ],
             ),
-            Positioned(
-              bottom: -40,
-              // left: 0,
+          ),
+          Positioned(
+            bottom: -45,
+            // left: 0,
+            child: Container(
+              padding: const EdgeInsets.all(20),
+              width: MediaQuery.of(context).size.width,
               child: TextFormField(
                 decoration: InputDecoration(
-                    prefixIcon: Icon(
+                    prefixIcon: const Icon(
                       Icons.search,
                       color: Colors.grey,
                     ),
-                    border: OutlineInputBorder(
+                    enabledBorder: OutlineInputBorder(
                         borderRadius: BorderRadius.circular(10),
-                        borderSide: BorderSide(
-                            width: 0,
+                        borderSide: const BorderSide(
+                            width: 10,
                             color: Colors.white,
                             style: BorderStyle.none)),
-                    fillColor: Colors.black,
+                    focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: const BorderSide(
+                            width: 10,
+                            color: Colors.white,
+                            style: BorderStyle.none)),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: const BorderSide(
+                            width: 10,
+                            color: Colors.white,
+                            style: BorderStyle.none)),
+                    fillColor: Colors.white,
                     filled: true,
                     hintText: "Search dish name, restaurant",
-                    contentPadding: EdgeInsets.all(10),
-                    hintStyle: TextStyle(fontSize: 14)),
+                    contentPadding: const EdgeInsets.all(10),
+                    hintStyle: const TextStyle(fontSize: 14)),
               ),
-            )
-          ]),
-        ));
+            ),
+          )
+        ]));
   }
 }
